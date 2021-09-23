335146c933b6c505497911781195080c14dedd86
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-09-23 08:50:32 -0300

    refactor: add binary draw method

--- src/infra/PBMMemoryFileContentDrawer.ts
+++ src/infra/PBMMemoryFileContentDrawer.ts
@@ -3,6 +3,8 @@ import { Resolution } from '@shared/types'
 import FileContentDrawer from '@app/contracts/FileContentDrawer'
 
 class PBMMemoryFileContentDrawer implements FileContentDrawer {
+	private readonly hexCodes = ['000000', 'FFFFFF']
+
 	private drawAsciiType(name: string, resolution: Resolution) {
 		const type = 'P1\n'
 		const headerComment = `# ${name}\n`
@@ -30,14 +32,21 @@ class PBMMemoryFileContentDrawer implements FileContentDrawer {
 		return type + headerComment + specs + pixelsMatrix
 	}
 
-	private drawBinaryType(name: string, resolution: Resolution) {
-		const type = 'P4\n'
-		const headerComment = `# ${name}\n`
-		const specs = `${resolution.width} ${resolution.height}\n`
+	private drawBinaryType(resolution: Resolution) {
+		const type = 'P4 '
+		const specs = `${resolution.width} ${resolution.height} `
 
-		const body = '00000079E79E41041271C71E41041041E790000000'
+		let body = ''
 
-		return type + headerComment + specs + body
+		for (let y = 0; y < resolution.height; y++) {
+			for (let x = 0; x < resolution.width; x++) {
+				const randomHexIndex = Math.round(Math.random())
+
+				body += this.hexCodes[randomHexIndex]
+			}
+		}
+
+		return type + specs + body
 	}
 
 	async execute({ name, resolution, codeType }: FileContentDrawer.Params) {
@@ -45,7 +54,7 @@ class PBMMemoryFileContentDrawer implements FileContentDrawer {
 			return this.drawAsciiType(name, resolution)
 		}
 
-		return this.drawBinaryType(name, resolution)
+		return this.drawBinaryType(resolution)
 	}
 }
 
